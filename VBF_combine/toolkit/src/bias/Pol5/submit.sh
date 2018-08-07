38233e381e87478a14096ab003e53c941c8c54bf
lata <panwarlsweet@gmail.com>
2018-08-07 23:40:57 +0200

    adding job submission script for toy generation and fitting the toy

--- VBF_combine/toolkit/src/bias/Pol5/submit.sh
+++ VBF_combine/toolkit/src/bias/Pol5/submit.sh
@@ -6,23 +6,23 @@ num=0
 max=50 #100
 n_toys=20  #10
 inj=_inj
-name=Pol5
-fit=Pol5
-mu=2
+name=sine
+fit=Pol5	
+mu=0
 #cat0=0
 #cat1=2
-cat0=3
-cat1=6
+cat0=0
+cat1=3
 
-datacard=/mnt/t3nfs01/data01/shome/nchernya/VBFHbb2016/VBF_fit/bias/datacards/datacard_bias${fit}_m125_CAT${cat0}-CAT${cat1}_CATveto.txt
+datacard=/afs/cern.ch/work/l/lata/VBF_Analysis/CMSSW_7_4_7/src/VBFHbb2016/VBF_combine/toolkit/src/set_DBT3P5_SBT2P7/datacards/datacard_vbfHbb_bias${name}_m125_CAT${cat0}-CAT${cat1}_CATveto.txt
 while [ $num -lt $max ]
 do
 #	toys_file=$(find /mnt/t3nfs01/data01/shome/nchernya/VBFHbb2016/VBF_fit/bias/toys/${name}/ -name higgsCombine${name}${num}_inj1_cat${cat0}-cat${cat1}.GenerateOnly.mH120.*.root)
-	toys_file=$(find /mnt/t3nfs01/data01/shome/nchernya/VBFHbb2016/VBF_fit/bias/toys/new_inj2/ -name higgsCombine${name}${num}_inj2_cat${cat0}-cat${cat1}.GenerateOnly.mH120.*.root)
-#	echo $toys_file  
-	qsub -q all.q batch.sh  $datacard  $toys_file  ${name}_fit${fit}$inj${mu}_cat${cat0}${cat1}_ $num $name $mu
+	toys_file=/afs/cern.ch/work/l/lata/VBF_Analysis/CMSSW_7_4_7/src/VBFHbb2016/VBF_combine/toolkit/src/bias/makeToys/${name}/higgsCombine${name}${num}_inj${mu}_cat${cat0}-cat${cat1}.GenerateOnly.mH120.*.root
+	bsub -q 1nd batch.sh  $datacard  $toys_file  ${name}_fit${fit}$inj${mu}_cat${cat0}${cat1}_ $num $name $mu
 #	qsub -q all.q batch2.sh  $datacard  $toys_file  ${name}_fit${fit}$inj${mu}_cat${cat0}${cat1}_ $num $name $mu
 	echo $num
+        echo $toys_file
 	num=$(( $num + 1 ))
 done
 
--- VBF_combine/toolkit/src/bias/makeToys/create_toys.sh
+++ VBF_combine/toolkit/src/bias/makeToys/create_toys.sh
@@ -1,20 +1,53 @@
-export WORKDIR=`pwd`
-echo "Working on a machine:" `uname -a`
-cd $WORKDIR
-
+cd /afs/cern.ch/work/l/lata/VBF_Analysis/CMSSW_7_4_7/src/VBFHbb2016/VBF_combine/toolkit/src/bias/makeToys
 num=0
-max=100
-n_toys=10
-name=Pol3
+max=50
+n_toys=20
+name_list="
+pol2_invdijet
+exp_pol2
+"
+name2=_sine
 inj=_inj
-mu=1
+mu=0
+## DB cats.
 cat0=0
-cat1=2
-datacard=/mnt/t3nfs01/data01/shome/nchernya/VBFHbb2016/VBF_fit/bias/datacards/datacard_bias${name}_m125_CAT${cat0}-CAT${cat1}_CATveto.txt
-while [ $num -lt $max ]
+cat1=3
+## SB cats.
+
+name_list2="
+modG4
+"
+cat2=4
+cat3=8
+
+
+for arg in $name_list:
 do
-	qsub -q short.q batch.sh  $n_toys $mu $name$num$inj${mu}_cat${cat0}-cat${cat1}  $datacard $name
-	echo $num
-	num=$(( $num + 1 ))
+  	name=$arg
+        datacard=/afs/cern.ch/work/l/lata/VBF_Analysis/CMSSW_7_4_7/src/VBFHbb2016/VBF_combine/toolkit/src/set_DBT3P5_SBT2P7/datacards/datacard_vbfHbb_bias${name}_m125_CAT${cat0}-CAT${cat1}_CATveto.txt
+        echo datacard
+        num=0
+        while [ $num -lt $max ]
+        do
+        bsub -q 1nh batch.sh  $n_toys $mu $name$num$inj${mu}_cat${cat0}-cat${cat1}  $datacard $name
+        echo $num $arg CAT${cat0} CAT${cat1} inj${mu}
+
+        num=$(( $num + 1 ))
+	done
 done
+
+
+#for arg in $name_list2:
+#do
+#        name=$arg
+#        datacard=/afs/cern.ch/work/l/lata/VBF_Analysis/CMSSW_7_4_7/src/VBFHbb2016/VBF_combine/toolkit/src/set_DBT3P5_SBT2P7/datacards/datacard_vbfHbb_bias${name}_m125_CAT${cat2}-CAT${cat3}_CATveto.txt
+#        num=0
+#        while [ $num -lt $max ]
+#        do
+#        bsub -q 1nd batch.sh  $n_toys $mu $name$num$inj${mu}_cat${cat2}-cat${cat3}  $datacard $name
+#        echo $num $arg CAT${cat2} CAT${cat3} inj${mu}
+
+#        num=$(( $num + 1 ))
+#        done
+#done
 
